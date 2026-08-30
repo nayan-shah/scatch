c7a6cb80effbe7f173b9d48ce7485d299497063c
nayan-shah <nayanshah130204@gmail.com>
2026-08-30 19:49:15 +0530

    separation of concerns

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express()
 const cookieparser = require('cookie-parser')
 const path = require('path')
+const db = require('./config/mongoose-connection')
+const ownerRouter = require('./routes/ownerRouter')
+const userRouter = require('./routes/userRouter')
+const productsRouter = require('./routes/productsRouter')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
@@ -9,9 +13,9 @@ app.use(cookieparser())
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => {
-    res.send('hii');
-})
+app.get('/user', userRouter);
+app.get('/product', productsRouter)
+app.get('/owner', ownerRouter)
 
 app.listen(3000, () => {
     console.log('your app is running on prot 3000')
